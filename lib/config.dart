95379ccb208c491cf3d70fc78738e9d256b8cf1f
JoshLiberato <lolijo12@gmail.com>
2022-05-16 13:14:38 -0500

    fs commit

---
+++ lib/config.dart
@@ -0,0 +1,10 @@
+import 'package:camera_deep_ar/camera_deep_ar.dart';
+
+DeepArConfig config = const DeepArConfig(
+  androidKey:
+      "8e0a63298536b221e66b01c95a77fe6a1c1523bac3e7a24a18d6d563fa4c68661b8cb17d52b3220d",
+  ioskey:
+      "53618212114fc16bbd7499c0c04c2ca11a4eed188dc20ed62a7f7eec02b41cb34d638e72945a6bf6",
+  displayMode: DisplayMode.camera,
+  // displayMode: DisplayMode.camera,
+);
